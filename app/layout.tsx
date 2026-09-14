04cdfe63672b4ea59f23567a36a89cee8844da35
paulthetiler <paul@commercialtiling.com>
2026-09-15 00:08:34 +0100

    Load estimator preview styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./brand.css";
+import "./estimator-preview.css";
 
 export const metadata: Metadata = {
   title: "FORM | Bathrooms, Wetrooms & Bespoke Tiling in Cheshire",
